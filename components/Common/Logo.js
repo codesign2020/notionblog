cdb471b405f30901fe8446706dd2c82803e26b2b
Sherlock Lau <liuz1226@hotmail.com>
2023-09-03 20:42:48 +0800

    Update Logo.js

--- components/Common/Logo.js
+++ components/Common/Logo.js
@@ -2,21 +2,31 @@
 import * as React from 'react'
 
 const Logo = (props) => (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    width='24'
-    height='24'
-    viewBox='0 0 100 100'
-    style={{
-      opacity: 1
-    }}
-    {...props}
-  >
-    <g>
-    <path d="M9.3906 2.48499H16.5009L11.7607 12.6764H4.65039L9.3906 2.48499Z" fill="black"/>
-    <path d="M13.4198 12.6765H20.5301L15.7899 22.8679H8.67957L13.4198 12.6765Z" fill="black"/>
-    </g>
-  </svg>
+  <img alt="" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjUiIGhlaWdodD0iMjUiIHZpZXdCb3g9IjAgMCAyNSAyNSIgZmlsbD0ibm9u
+ZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPGcgY2xpcC1wYXRoPSJ1cmwo
+I2NsaXAwXzVfMikiPgo8cGF0aCBkPSJNOS4zOTA2IDIuNDg0OTlIMTYuNTAwOUwxMS43NjA3IDEy
+LjY3NjRINC42NTAzOUw5LjM5MDYgMi40ODQ5OVoiIGZpbGw9ImJsYWNrIi8+CjxwYXRoIGQ9Ik05
+LjA1MTIgMjIuNjMwOUwxMy41NzA5IDEyLjkxMzVIMjAuMTU4NUwxNS42Mzg3IDIyLjYzMDlIOS4w
+NTEyWiIgZmlsbD0id2hpdGUiIHN0cm9rZT0iYmxhY2siIHN0cm9rZS13aWR0aD0iMC40NzQwMjEi
+Lz4KPC9nPgo8ZGVmcz4KPGNsaXBQYXRoIGlkPSJjbGlwMF81XzIiPgo8cmVjdCB3aWR0aD0iMjQi
+IGhlaWdodD0iMjQiIGZpbGw9IndoaXRlIiB0cmFuc2Zvcm09InRyYW5zbGF0ZSgwLjUgMC41KSIv
+Pgo8L2NsaXBQYXRoPgo8L2RlZnM+Cjwvc3ZnPgo=">
+
+  // <svg
+  //   xmlns='http://www.w3.org/2000/svg'
+  //   width='24'
+  //   height='24'
+  //   viewBox='0 0 100 100'
+  //   style={{
+  //     opacity: 1
+  //   }}
+  //   {...props}
+  // >
+  //   <g>
+  //   <path d="M9.3906 2.48499H16.5009L11.7607 12.6764H4.65039L9.3906 2.48499Z" fill="black"/>
+  //   <path d="M13.4198 12.6765H20.5301L15.7899 22.8679H8.67957L13.4198 12.6765Z" fill="black"/>
+  //   </g>
+  // </svg>
 )
 
 export default Logo
