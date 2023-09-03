e3c3f1507f3dbd6e124267c030f15490b1436d4b
Sherlock Lau <liuz1226@hotmail.com>
2023-09-03 17:43:30 +0800

    Update Logo.js

--- components/Common/Logo.js
+++ components/Common/Logo.js
@@ -4,8 +4,8 @@ import * as React from 'react'
 const Logo = (props) => (
   <svg
     xmlns='http://www.w3.org/2000/svg'
-    width='36'
-    height='36'
+    width='24'
+    height='24'
     viewBox='0 0 100 100'
     style={{
       opacity: 1
@@ -13,14 +13,14 @@ const Logo = (props) => (
     {...props}
   >
     <g clip-path="url(#clip0_5_2)">
-      <path d="M53 24H83L63 67H33L53 24Z" fill="black" />
-      <path d="M51.568 109L70.6378 68H98.432L79.3622 109H51.568Z" fill="white" stroke="black" stroke-width="2" />
-    </g>
-    <defs>
-      <clipPath id="clip0_5_2">
-        <rect width="133" height="133" fill="white" />
-      </clipPath>
-    </defs>
+<path d="M49.2687 11H88.6716L62.403 67.4776H23L49.2687 11Z" fill="black"/>
+<path d="M47.3878 122.642L72.4347 68.791H108.941L83.8937 122.642H47.3878Z" stroke="black" stroke-width="2.62687"/>
+</g>
+<defs>
+<clipPath id="clip0_5_2">
+<rect width="133" height="133" fill="white"/>
+</clipPath>
+</defs>
   </svg>
 )
 
